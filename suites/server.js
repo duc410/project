2043733d6f96c91aa4975d96f7365775a3e019c3
duc410 <54860234+duc410@users.noreply.github.com>
2019-11-30 20:53:18 +0700

    add
    almost done

--- suites/server.js
+++ suites/server.js
@@ -6,6 +6,9 @@ app.use('/css', express.static(__dirname + '/css'));
 app.use('/fonts', express.static(__dirname + '/fonts'));
 app.use('/images', express.static(__dirname + '/images'));
 app.use('/scss', express.static(__dirname + '/scss'));
+app.use('/hotelfiles', express.static(__dirname + '/hotelfiles'));
+app.use('/Hotel-Hotel WordPress Theme_files', express.static(__dirname + '/Hotel-Hotel WordPress Theme_files'));
+app.use('/node-modules', express.static(__dirname + '/node-modules'));
 
 app.get('/', function (req, res) {
    res.sendFile("index.html", { root : __dirname});
@@ -28,11 +31,20 @@ app.get('/events', function (req, res) {
 app.get('/rooms', function (req, res) {
    res.sendFile("rooms.html", { root : __dirname});
 })
-app.get('/book', function (req, res) {
-   res.sendFile("book.html", { root : __dirname});
-})
 app.get('/room-informations', function (req, res) {
    res.sendFile("room-informations.html", { root : __dirname});
+})
+app.get('/rooms2', function (req, res) {
+   res.sendFile("rooms2.html", { root : __dirname});
+})
+app.get('/rooms3', function (req, res) {
+   res.sendFile("rooms3.html", { root : __dirname});
+})
+app.get('/rooms4', function (req, res) {
+   res.sendFile("rooms4.html", { root : __dirname});
+})
+app.get('/rooms5', function (req, res) {
+   res.sendFile("rooms5.html", { root : __dirname});
 })
 
 var server = app.listen(8000, function () {
